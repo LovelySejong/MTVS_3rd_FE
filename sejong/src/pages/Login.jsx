2f7fc9716c283664f88700af36d2e7e0e0dc68e4
INUK-ai <arheon2469@gmail.com>
2024-09-22 11:49:37 +0900

    Feat: api -auth.js

--- sejong/src/pages/Login.jsx
+++ sejong/src/pages/Login.jsx
@@ -1,15 +1,19 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const navigate = useNavigate();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const response = await axios.post('/api/login', { email, password });
       console.log('Login Success:', response.data);
+      navigate('/mypage');
     } catch (error) {
       console.error('Login Error:', error);
     }
