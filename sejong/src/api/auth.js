4ffd31baab67551fd7f2dcbdb52e9f0330f4a77f
INUK-ai <arheon2469@gmail.com>
2024-09-22 12:37:09 +0900

    Refactor: storeToken

--- sejong/src/api/auth.js
+++ sejong/src/api/auth.js
@@ -2,14 +2,12 @@ import axios from 'axios';
 
 const baseURL = import.meta.env.VITE_BASE_URL;
 
-const storeTokens = (authorization) => {
-
-  if (authorization && authorization.startsWith('Bearer ')) {
-    const token = authorization.split(' ')[1];
+const storeTokens = (token) => {
+  if (token) {
     localStorage.setItem('token', token);
     console.log('토큰 저장 성공:', token);
   } else {
-    console.error('Authorization 헤더에 토큰이 없습니다.');
+    console.error('토큰이 없습니다.');
   }
 };
 
@@ -38,11 +36,10 @@ export const login = async (email, password) => {
   };
 
   const res = await axios.post(`${baseURL}/auth/login`, loginDTO);
-
+  
   // 헤더에서 토큰 정보 추출
-  const token = res.headers['Authorization'] || res.headers['authorization'];
+  const token = res.data.response.accessToken;
 
-  console.log(res.headers);
   console.log('token: ' + token);
 
   storeTokens(token);
