6881860bf10e4655bfaba797723509d2b32ac25c
kr-nius <ybyb414@naver.com>
2024-09-26 16:57:48 +0900

    방탈출 전적 조회 api 연결

--- sejong/src/api/record.js
+++ sejong/src/api/record.js
@@ -1,10 +1,19 @@
+import axios from "axios";
+
+// 토큰을 추출하는 함수 (예: 로컬스토리지에서)
+const getToken = () => {
+  return localStorage.getItem("token");
+};
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
 // 퀴즈 평균 점수 가져오기
 export const getQuizAverageScores = async () => {
   return [
-    { part: '단어', averageScore: 85 },
-    { part: '한자어', averageScore: 75 },
-    { part: '띄어쓰기', averageScore: 90 },
-    { part: '문해력', averageScore: 70 }
+    { part: "단어", averageScore: 85 },
+    { part: "한자어", averageScore: 75 },
+    { part: "띄어쓰기", averageScore: 90 },
+    { part: "문해력", averageScore: 70 },
   ];
 };
 
@@ -14,7 +23,7 @@ export const getUserEscapeTime = async () => {
     roomNumber1: 150,
     roomNumber2: 160,
     roomNumber3: 155,
-    roomNumber4: 175
+    roomNumber4: 175,
   };
 };
 
@@ -23,33 +32,48 @@ export const getEscapeRoomRecords = async () => {
   return [
     {
       gameId: 1,
-      partner: 'user2',
+      partner: "user2",
       rooms: [
-        { roomName: '단어의 방', escapeTime: 150 },
-        { roomName: '한자어의 방', escapeTime: 160 },
-        { roomName: '띄어쓰기의 방', escapeTime: 155 },
-        { roomName: '문해력의 방', escapeTime: 175 }
-      ]
+        { roomName: "단어의 방", escapeTime: 150 },
+        { roomName: "한자어의 방", escapeTime: 160 },
+        { roomName: "띄어쓰기의 방", escapeTime: 155 },
+        { roomName: "문해력의 방", escapeTime: 175 },
+      ],
     },
     {
       gameId: 2,
-      partner: 'user3',
+      partner: "user3",
       rooms: [
-        { roomName: '단어의 방', escapeTime: 140 },
-        { roomName: '한자어의 방', escapeTime: 150 },
-        { roomName: '띄어쓰기의 방', escapeTime: 145 },
-        { roomName: '문해력의 방', escapeTime: 160 }
-      ]
+        { roomName: "단어의 방", escapeTime: 140 },
+        { roomName: "한자어의 방", escapeTime: 150 },
+        { roomName: "띄어쓰기의 방", escapeTime: 145 },
+        { roomName: "문해력의 방", escapeTime: 160 },
+      ],
     },
     {
       gameId: 3,
-      partner: 'user4',
+      partner: "user4",
       rooms: [
-        { roomName: '단어의 방', escapeTime: 128 },
-        { roomName: '한자어의 방', escapeTime: 187 },
-        { roomName: '띄어쓰기의 방', escapeTime: 176 },
-        { roomName: '문해력의 방', escapeTime: 142 }
-      ]
-    }
+        { roomName: "단어의 방", escapeTime: 128 },
+        { roomName: "한자어의 방", escapeTime: 187 },
+        { roomName: "띄어쓰기의 방", escapeTime: 176 },
+        { roomName: "문해력의 방", escapeTime: 142 },
+      ],
+    },
   ];
+};
+
+export const getTestRecord = async () => {
+  try {
+    const token = getToken();
+    const res = await axios.get(`${BASE_URL}/log`, {
+      headers: {
+        Authorization: `Bearer ${token}`, // Authorization 헤더에 토큰 추가
+      },
+    });
+    console.log("전적 조회 성공: ", res.data);
+    return res.data;
+  } catch (error) {
+    console.log("전적 조회 실패: ", error);
+  }
 };
--- sejong/src/pages/Mypage.jsx
+++ sejong/src/pages/Mypage.jsx
@@ -1,12 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { Doughnut } from 'react-chartjs-2';
-import { Chart, ArcElement, Tooltip, Legend } from 'chart.js'; // Chart.js 요소 가져오기
-import { getUserEscapeTime, getQuizAverageScores, getEscapeRoomRecords } from '../api/record';
+import React, { useState, useEffect } from "react";
+import { Doughnut } from "react-chartjs-2";
+import { Chart, ArcElement, Tooltip, Legend } from "chart.js"; // Chart.js 요소 가져오기
+import {
+  getUserEscapeTime,
+  getQuizAverageScores,
+  getEscapeRoomRecords,
+  getTestRecord,
+} from "../api/record";
 import HeaderBar from "../components/Header";
-import { getUserProfile } from '../api/user';
-import '../css/Mypage.css';
+import { getUserProfile } from "../api/user";
+import "../css/Mypage.css";
 
-import profileImg from '../assets/images/432842383.jpg'
+import profileImg from "../assets/images/432842383.jpg";
 
 Chart.register(ArcElement, Tooltip, Legend);
 
@@ -14,16 +19,18 @@ Chart.register(ArcElement, Tooltip, Legend);
 const formatTime = (seconds) => {
   const minutes = Math.floor(seconds / 60);
   const remainingSeconds = seconds % 60;
-  return `${minutes}분 ${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}초`;
+  return `${minutes}분 ${
+    remainingSeconds < 10 ? "0" : ""
+  }${remainingSeconds}초`;
 };
 
 const Mypage = () => {
-  const [nickname, setNickname] = useState(''); // 닉네임 상태 추가
+  const [nickname, setNickname] = useState(""); // 닉네임 상태 추가
   const [averageTime, setAverageTime] = useState(null); // 방탈출 평균 시간 데이터 (초기값 null)
-  const [quizScores, setQuizScores] = useState([]);   // 퀴즈 평균 점수 데이터
+  const [quizScores, setQuizScores] = useState([]); // 퀴즈 평균 점수 데이터
   const [escapeRecords, setEscapeRecords] = useState([]); // 방탈출 기록 데이터
   const [loading, setLoading] = useState(true); // 로딩 상태
-  const [error, setError] = useState(null);    // 에러 상태
+  const [error, setError] = useState(null); // 에러 상태
 
   // 닉네임을 받아오는 API 호출
   const getProfile = async () => {
@@ -32,10 +39,10 @@ const Mypage = () => {
       if (response?.success && response?.response?.nickname) {
         setNickname(response.response.nickname); // 닉네임만 상태에 저장
       } else {
-        console.error('Invalid profile response:', response);
+        console.error("Invalid profile response:", response);
       }
     } catch (err) {
-      console.error('Failed to fetch profile:', err);
+      console.error("Failed to fetch profile:", err);
     }
   };
 
@@ -52,9 +59,12 @@ const Mypage = () => {
       const recordsData = await getEscapeRoomRecords();
       setEscapeRecords(recordsData);
 
+      const testData = await getTestRecord();
+      console.log("testData: ", testData);
+
       await getProfile(); // 닉네임 가져오기
     } catch (err) {
-      setError('데이터를 불러오는 중 오류가 발생했습니다.'); // 오류 메시지 설정
+      setError("데이터를 불러오는 중 오류가 발생했습니다."); // 오류 메시지 설정
     } finally {
       setLoading(false); // 로딩 상태 종료
     }
@@ -66,15 +76,24 @@ const Mypage = () => {
   }, []);
 
   // 원형 차트 데이터 (방탈출 평균 시간)
-  const doughnutData = averageTime ? {
-    labels: ['단어의 방', '한자어의 방', '띄어쓰기의 방', '문해력의 방'], // 방 이름으로 변경
-    datasets: [{
-      label: 'Escape Time (sec)',
-      data: [averageTime.roomNumber1, averageTime.roomNumber2, averageTime.roomNumber3, averageTime.roomNumber4], // API로부터 받은 데이터 사용
-      backgroundColor: ['#ffcd56', '#ff6384', '#36a2eb', '#fd6b19'],
-      hoverOffset: 4
-    }]
-  } : null; // 데이터가 없으면 null 처리
+  const doughnutData = averageTime
+    ? {
+        labels: ["단어의 방", "한자어의 방", "띄어쓰기의 방", "문해력의 방"], // 방 이름으로 변경
+        datasets: [
+          {
+            label: "Escape Time (sec)",
+            data: [
+              averageTime.roomNumber1,
+              averageTime.roomNumber2,
+              averageTime.roomNumber3,
+              averageTime.roomNumber4,
+            ], // API로부터 받은 데이터 사용
+            backgroundColor: ["#ffcd56", "#ff6384", "#36a2eb", "#fd6b19"],
+            hoverOffset: 4,
+          },
+        ],
+      }
+    : null; // 데이터가 없으면 null 처리
 
   return (
     <>
@@ -82,22 +101,30 @@ const Mypage = () => {
       <div className="mypage-container">
         {/* 프로필 섹션 */}
         <div className="profile-section">
-          <img src={profileImg} alt="Profile" className="profile-picture" /> {/* 하드코딩된 프로필 사진 */}
+          <img src={profileImg} alt="Profile" className="profile-picture" />{" "}
+          {/* 하드코딩된 프로필 사진 */}
           <h2>{nickname}</h2> {/* 닉네임 표시 */}
-          <button onClick={refreshData} className="refresh-button">갱신</button> {/* 갱신 버튼 */}
+          <button onClick={refreshData} className="refresh-button">
+            갱신
+          </button>{" "}
+          {/* 갱신 버튼 */}
         </div>
 
         {/* 로딩 상태 처리 */}
         {loading ? (
           <p>Loading data...</p>
-        ) : error ? ( 
-          <p>{error}</p>  // 에러 메시지 표시
+        ) : error ? (
+          <p>{error}</p> // 에러 메시지 표시
         ) : (
           <>
             <div className="stats-container">
               <div className="chart-section">
                 <h2>평균 방탈출 시간</h2>
-                {doughnutData ? <Doughnut data={doughnutData} /> : <p>데이터가 없습니다.</p>}
+                {doughnutData ? (
+                  <Doughnut data={doughnutData} />
+                ) : (
+                  <p>데이터가 없습니다.</p>
+                )}
               </div>
 
               <div className="score-section">
@@ -139,7 +166,9 @@ const Mypage = () => {
                   <h3>게임 ID: {record.gameId}</h3>
                   <p>함께 한 사람: {record.partner}</p>
                   {record.rooms.map((room, roomIndex) => (
-                    <p key={roomIndex}>{room.roomName}: {formatTime(room.escapeTime)}</p>
+                    <p key={roomIndex}>
+                      {room.roomName}: {formatTime(room.escapeTime)}
+                    </p>
                   ))}
                 </div>
               ))
